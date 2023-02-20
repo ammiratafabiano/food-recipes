cd9903cb404e575b22ff98cec69288e8d5a1bc98
ammiratafabiano <ammiratafabiano@gmail.com>
2023-02-21 00:37:43 +0100

    removed module not found

--- src/app/pages/planning/planning.page.spec.ts
+++ src/app/pages/planning/planning.page.spec.ts
@@ -1,8 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { PlanningPage } from './planning.page';
 
 describe('PlanningPage', () => {
@@ -12,7 +10,7 @@ describe('PlanningPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PlanningPage],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PlanningPage);
