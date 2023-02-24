4cac3258480ca47345c77bec2543828ca1238975
ammiratafabiano <ammiratafabiano@gmail.com>
2023-02-24 16:05:01 +0100

    add back button handler for android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import * as moment from 'moment';
+import { LoggingService } from './services/logging.service';
+import { NavigationService } from './services/navigation.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +11,12 @@ import * as moment from 'moment';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
+  constructor(
+    private translate: TranslateService,
+    private platform: Platform,
+    private loggingService: LoggingService,
+    private navigationService: NavigationService
+  ) {
     const currentLang = this.translate.getBrowserLang() || "en";
     this.translate.setDefaultLang(currentLang);
     moment.updateLocale(currentLang, {
@@ -16,5 +24,9 @@ export class AppComponent {
         dow: 1
       }
     })
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      this.loggingService.Info("AtomHeader", "Hardware Back Button", "pressed");
+      this.navigationService.pop();
+    });
   }
 }
