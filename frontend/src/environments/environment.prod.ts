1424b43e52c3a39a29befeab61275eca4e769db1
ammiratafabiano <ammiratafabiano@gmail.com>
2026-03-01 23:08:09 +0100

    changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.recipes.ammiratafabiano.dev',
-  googleClientId: '944834541951-fo75ek6gkutu5hh86tghmqlciua5hme9.apps.googleusercontent.com',
+  googleClientId: 'GOOGLE_CLIENT_ID_PLACEHOLDER',
   siteUrl: 'https://recipes.ammiratafabiano.dev',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
-  googleClientId: '944834541951-fo75ek6gkutu5hh86tghmqlciua5hme9.apps.googleusercontent.com',
+  googleClientId: 'GOOGLE_CLIENT_ID_PLACEHOLDER',
   siteUrl: 'http://localhost:8100',
 };
